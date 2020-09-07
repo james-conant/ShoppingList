0b44053ea204699eec4a5dbd110e03fc3c964b72
james-conant <jconant1993@gmail.com>
2020-09-06 20:32:30 -0500

    Boilerplate

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {flex: 1},
+});
+
+export default App;
